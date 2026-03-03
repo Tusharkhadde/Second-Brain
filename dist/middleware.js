cd87cf8e33902623458cb67b87c17d531445637b
Tusharkhadde <tusharkhadde06@gmail.com>
2026-03-03 11:13:29 +0530

    Added user middleware

--- dist/middleware.js
+++ dist/middleware.js
@@ -1,1 +1,29 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userMiddleware = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const config_1 = require("./config");
+const userMiddleware = (req, res, next) => {
+    try {
+        const header = req.headers["authorization"];
+        if (!header) {
+            return res.status(401).json({ message: "Authorization header missing" });
+        }
+        const token = header.split(" ")[1]; // Bearer TOKEN
+        const decoded = jsonwebtoken_1.default.verify(token, config_1.JWT_PASSWORD);
+        if (decoded) {
+            req.userId = decoded.userId;
+            next();
+        }
+        else {
+            return res.status(403).json({ message: "Invalid token" });
+        }
+    }
+    catch (err) {
+        return res.status(403).json({ message: "Invalid or expired token" });
+    }
+};
+exports.userMiddleware = userMiddleware;
